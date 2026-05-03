25caf3aef3811cd3b2313f023b4ea982bfc4148c
Kolchi <kolchiibrahim@icloud.com>
2026-05-03 18:19:51 +0400

    Add initial CalixyAI SwiftUI app

---
+++ CalixyAI/CalixyAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalixyAIApp.swift
+//  CalixyAI
+//
+//  Created by Ibrahim Kolchi on 03.05.26.
+//
+
+import SwiftUI
+
+@main
+struct CalixyAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
